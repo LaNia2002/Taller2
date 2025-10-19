6c85c69285eef6d0d8c2410b1553b3ab6c2122e7
LaNia2002 <estefania.ulloa@alumnos.ucn.cl>
2025-10-19 19:53:04 -0300

    Add SparseMatrix class definition

---
+++ SparseMatrix.h
@@ -0,0 +1,17 @@
+#pragma once 
+#include "NodeX.h" 
+class SparseMatrix { 
+    private: 
+        NodeX* start; 
+    public: 
+        SparseMatrix(); 
+        NodeX* getStart();
+        void setStart(NodeX*);
+        void add(int value, int xPos, int yPos); 
+        int get(int xPos, int yPos); 
+        void remove(int xPos, int yPos); 
+        void printStoredValues(); 
+        int density(); 
+        SparseMatrix* multiply(SparseMatrix* second); 
+        ~SparseMatrix(); 
+}; 
