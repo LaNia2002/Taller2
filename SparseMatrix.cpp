61bbcc292f81ec477aff81bcd03534c1f5a81756
Jossette12 <jossette.doll@alumnos.ucn.cl>
2025-10-23 12:41:59 -0300

    Implement add and get methods for SparseMatrix

--- SparseMatrix.cpp
+++ SparseMatrix.cpp
@@ -3,8 +3,64 @@ using namespace std;
 #include <iostream>
 #include <string.h>
 
-        void add(int value, int xPos, int yPos); 
-        int get(int xPos, int yPos); 
+         void SparseMatrix::add(int value, int xPos, int yPos) {
+                if (value == 0) return; 
+                NodeX* actualX = start;
+                NodeX* prevX = nullptr;
+
+                while (actualX != nullptr && actualX->x < xPos) {
+                        prevX = actualX;
+                        actualX = actualX->next;}
+
+                if (actualX == nullptr || actualX->x != xPos) {
+
+                        NodeX* nuevoX = new NodeX(xPos);
+                        if (prevX == nullptr) {
+                        nuevoX->next = start;
+                        start = nuevoX;} 
+                        else {
+                        nuevoX->next = actualX;
+                        prevX->next = nuevoX;}
+                        actualX = nuevoX;}
+
+                NodeY* actualY = actualX->col;
+                NodeY* prevY = nullptr;
+
+                while (actualY != nullptr && actualY->y < yPos) {
+                        prevY = actualY;
+                        actualY = actualY->next;}
+
+                if (actualY != nullptr && actualY->y == yPos) {
+                        actualY->value = value;} 
+
+                        else {
+                        NodeY* nuevoY = new NodeY(yPos, value);
+                        if (prevY == nullptr) {
+                        nuevoY->next = actualX->col;
+                        actualX->col = nuevoY;
+                        } else {
+                        nuevoY->next = actualY;
+                        prevY->next = nuevoY;}
+                }}
+
+        int SparseMatrix::get(int xPos, int yPos) {
+                NodeX* actualX = start;
+
+                while (actualX != nullptr && actualX->x < xPos){
+                        actualX = actualX->next;}
+
+                if (actualX == nullptr || actualX->x != xPos)
+                        return 0;
+
+                NodeY* actualY = actualX->col;
+                while (actualY != nullptr && actualY->y < yPos){
+                        actualY = actualY->next;}
+
+                if (actualY == nullptr || actualY->y != yPos)
+                        return 0;
+
+                return actualY->value;}
+
         void remove(int xPos, int yPos); 
         void printStoredValues(); 
         int density(); 
