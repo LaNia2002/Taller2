ac585578cd206f8c0069f2a7af5e827f20058248
Jossette12 <jossette.doll@alumnos.ucn.cl>
2025-10-23 19:50:03 -0300

    Implement menu for SparseMatrix operations
    Added a menu system for matrix operations.

--- Main.cpp
+++ Main.cpp
@@ -5,13 +5,62 @@
 
 int main()
 {
-
   SparseMatrix* matriz = new SparseMatrix();
-  NodeX* nodo1 = new NodeX();
-  matriz -> setStart(nodo1);
-  NodeY* nodoc = new NodeY(2);
-  matriz -> getStart() -> col = nodoc;
-  std::cout << matriz -> getStart() -> col -> value << std::endl;
+  int opcion;
+  do{
+
+    cout<<"Menu Matriz"<<endl;
+    cout<<""<<endl;
+    cout<<"1) Agregar valor"<<endl;
+    cout<<"2) Obtener valor"<<endl;
+    cout<<"3) Eliminar valor"<<endl;
+    cout<<"4) Imprimir valores almacenados"<<endl;
+    cout<<"5) Calcular densidad"<<endl;
+    cout<<"6) Multiplicar matrices"<<endl;
+    cout<<"7.) Salir"<<endl;
+
+    cin>>opcion;
+
+    if(opcion==1){
+      int valor, xPos, yPos;
+      cout<<"Ingrese el valor a agregar: ";
+      cin>>valor;
+      cout<<"Ingrese la posicion x: ";
+      cin>>xPos;
+      cout<<"Ingrese la posicion y: ";
+      cin>>yPos;
+      matriz -> add(valor, xPos, yPos);
+    }
+
+    else if(opcion==2){
+      int xPos, yPos;
+      cout<<"Ingrese la posicion x: ";
+      cin>>xPos;
+      cout<<"Ingrese la posicion y: ";
+      cin>>yPos;
+      cout<<"El valor en la posicion ("<<xPos<<","<<yPos<<") es: "<<matriz -> get(xPos, yPos)<<endl;
+    }
+
+    else if(opcion==3){
+      int xPos, yPos;
+      cout<<"Ingrese la posicion x: ";
+      cin>>xPos;
+      cout<<"Ingrese la posicion y: ";
+      cin>>yPos;
+      matriz -> remove(xPos, yPos);}
+
+    else if(opcion==4){
+      matriz -> printStoredValues();}
+
+    else if(opcion==5){
+      cout<<"La densidad de la matriz es: "<<matriz -> density()<<"%"<<endl;}
+
+    else if(opcion==6){
+    }
+
+  }while(opcion>=1 && opcion<7);
+
+
   
   return 0;
 }
